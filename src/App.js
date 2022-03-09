adfe5b628931af0e538bd94907ae2346768c389a
Kishan Prajapat <47397967+kishanprajapat2564788@users.noreply.github.com>
2022-03-09 11:40:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+
+const API = {
+  key: "58bdcb533b890655cbc2603ca38c6464",
+  URl: "http://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${API.URl}weather?q=${inputValue}&units=metric&APPID=${API.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setInputValue('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // let today = new Date();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    // let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    return `${day} ${date} ${month} ${year} `
+  }
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm': (weather.main.temp < 0) ? 'app soCold': 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search here..."
+            onChange={e => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
